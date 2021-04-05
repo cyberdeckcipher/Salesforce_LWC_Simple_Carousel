7fef94c45506260de298059f9f0e17b2486fbfa9
Ulisses Sigma <ulisses.sigma@gmail.com>
2021-04-05 13:09:13 -0300

    :hammer: removing unecessary object reference.

--- lwc/adopetCaroselWrapper/adopetCaroselWrapper.js
+++ lwc/adopetCaroselWrapper/adopetCaroselWrapper.js
@@ -1,10 +1,10 @@
 import { LightningElement } from 'lwc';
-import CAROUSEL_IMAGES from '@salesforce/resourceUrl/Pet';
+import CAROUSEL_IMAGES from '@salesforce/resourceUrl/Carousel';
 
 export default class AdopetCaroselWrapper extends LightningElement {
     recordId = () =>{
         var splitedurl  = document.URL.split("/");
-        return splitedurl[splitedurl.indexOf("Pet__c") + 1];
+        return splitedurl[splitedurl.indexOf("ObjectBoundCarousel__c") + 1];
     }
 
     slides = [
